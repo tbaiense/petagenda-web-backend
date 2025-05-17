716e25740d8f067f2e9810308a425e5d8abf93fc
tbaiense <tbaiense@proton.me>
2025-05-17 14:11:14 -0300

    separa lógica de parse e validação em função distinta

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { createHmac } = require('node:crypto');
+const { parseJWT, verifyJWT } = require('../utils/jwt');
 
 
-
-router.post('/verify-token', (req, res, next) => {
+router.post('/verify-token', parseJWT, (req, res, next) => {
     function reject(msg) {
         res.status(400).json({
             success: false,
@@ -14,57 +14,19 @@ router.post('/verify-token', (req, res, next) => {
     }
 
     try {
-        const jwt = req.headers["authorization"].split(" ")[1];
-
-        // Verificando signature
-        const [ header, payload, signature ] = jwt.split('.');
-
-        const headerObj = JSON.parse(Buffer.from(header, 'base64url').toString());
-        const payloadObj = JSON.parse(Buffer.from(payload, 'base64url').toString());
-
-        if (headerObj.alg !== 'HS256') {
-            reject('Algoritmo de hash inválido');
-            return;
-        }
-
-        const hmac = createHmac('sha256', 'petagenda');
-        hmac.update(header + "." + payload);
-        const digest = hmac.digest('base64url');
-
-        console.log('authHeader: ', jwt);
-        let errMsg;
-
-        if (digest !== signature) {
-            reject('signature diferente');
-            return;
-        }
-
-        if (payloadObj.iss !== "petagenda-backend") {
-            reject('issuer desconhecido');
-            return;
-        }
-
-        if (payloadObj.exp <= Math.floor(Date.now() / 1000)) {
-            reject('token expirado');
-            return;
-        }
-
-        if (payloadObj.token !== '123token') {
-            reject('token inválido');
-            return;
-        }
+        verifyJWT(req.jwt);
 
         console.log('jwt aceito!');
         res.json({
             success: true,
             message: 'JWT aceito',
-            user: payloadObj.user,
-            admin: payloadObj.admin
+            user: req.jwt.payload.user,
+            admin: req.jwt.payload.admin
         });
 
         return;
     } catch (err) {
-        console.log('jwt rejeitado! ', headerObj, payloadObj);
+        console.log('jwt rejeitado! ', req.jwt.header, req.jwt.payload);
         next(err);
     }
 });
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,3 +1,61 @@
-exports.parse = function (jwt) {
+const { createHmac } = require('node:crypto');
 
-}
+exports.parseJWT = function (req, res, next) {
+    const authHeader = req.headers["authorization"];
+
+    if (authHeader) {
+        const encoded = authHeader.split(" ")[1];
+
+        // Verificando signature
+        const parts = encoded.split('.');
+        const [ header, payload, signature ] = parts;
+
+        const headerObj = JSON.parse(Buffer.from(header, 'base64url').toString());
+        const payloadObj = JSON.parse(Buffer.from(payload, 'base64url').toString());
+
+        if (headerObj.alg !== 'HS256') {
+            next(new Error('Algoritmo de hash inválido'));
+            return;
+        }
+
+        const jwt = {
+            encoded: encoded,
+            header: {
+                encoded: header,
+                ...headerObj
+            },
+            payload: {
+                encoded: payload,
+                ...payloadObj
+            },
+            signature: signature
+        };
+
+        req.jwt = jwt;
+    }
+
+    next();
+};
+
+exports.verifyJWT = function (jwt) {
+
+    const hmac = createHmac('sha256', 'petagenda');
+    hmac.update(jwt.header.encoded + "." + jwt.payload.encoded);
+    const digest = hmac.digest('base64url');
+
+    if (digest !== jwt.signature) {
+        throw new Error('signature diferente');
+    }
+
+    if (jwt.payload.iss !== "petagenda-backend") {
+        throw new Error('issuer desconhecido');
+    }
+
+    if (jwt.payload.exp <= Math.floor(Date.now() / 1000)) {
+        throw new Error('token expirado');
+    }
+
+    if (jwt.payload.token !== '123token') { // TODO: buscar no banco o token
+        throw new Error('token inválido');
+    }
+};
