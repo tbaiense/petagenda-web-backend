4a7b4584943eb98fe96aec681ab5c1565792aee5
Thiago Moura Baiense <tbaiese@proton.me>
2025-06-25 17:57:55 -0300

    corrige pesquisa de agendamentos e serviços realizados

--- models/agendamento.js
+++ models/agendamento.js
@@ -600,10 +600,23 @@ class Agendamento {
                         filterSQL += `${(filter.option) ? 'AND' : ''} estado = '${filter.estado}'`;
                     }
 
-                    sql = `SELECT * FROM vw_agendamento WHERE ${filterSQL} ORDER BY ${orderSQL} LIMIT ${limit} OFFSET ${limit * page}`;
+                    sql = 'SELECT '
+                            + "COUNT(a.id) OVER() AS qtd_agendamento, "
+                            + "a.id AS id_agendamento, "
+                            + "a.dt_hr_marcada AS dt_hr_marcada, "
+                            + "a.estado AS estado, "
+                            + "a.id_pacote_agend AS id_pacote_agend, "
+                            + "a.valor_servico AS valor_servico, "
+                            + "a.valor_total AS valor_total, "
+                            + "a.id_servico_realizado AS id_servico_realizado, "
+                            + "i_s.* "
+                        + "FROM agendamento AS a "
+                        + "INNER JOIN vw_info_servico AS i_s ON (i_s.id_info_servico = a.id_info_servico) "
+                        + `WHERE ${filterSQL} ORDER BY ${orderSQL} `
+                        + `LIMIT ${limit} OFFSET ${limit * page}`;
+
                     params = [filter.query];
 
-                    console.log('sql ', sql);
 
                 } else {
                     sql = `SELECT * FROM vw_agendamento ORDER BY id_agendamento DESC LIMIT ${limit} OFFSET ${limit * page}`;
--- models/servicoRealizado.js
+++ models/servicoRealizado.js
@@ -470,7 +470,19 @@ class ServicoRealizado {
                         default: {}
                     }
 
-                    sql = `SELECT * FROM vw_servico_realizado WHERE ${filterSQL} ORDER BY ${orderSQL} LIMIT ${limit} OFFSET ${limit * page}`;
+                    sql = 'SELECT '
+                            + 'COUNT(s_r.id) OVER() AS qtd_servico_realizado, '
+                            + 's_r.id AS id_servico_realizado, '
+                            + 's_r.dt_hr_inicio AS dt_hr_inicio, '
+                            + 's_r.dt_hr_fim AS dt_hr_fim, '
+                            + 's_r.valor_servico AS valor_servico, '
+                            + 's_r.valor_total AS valor_total, '
+                            + 'i_s.* '
+                        + 'FROM servico_realizado AS s_r '
+                            + 'INNER JOIN vw_info_servico AS i_s ON (i_s.id_info_servico = s_r.id_info_servico) '
+                        + `WHERE ${filterSQL} `
+                        + `ORDER BY ${orderSQL}, dt_hr_fim DESC LIMIT ${limit} OFFSET ${limit * page}`;
+
                     params = [filter.query];
 
                     console.log('sql ', sql);
